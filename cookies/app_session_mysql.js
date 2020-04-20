f2a91ad8cfdb395c2b124517bf25302af0767acd
dk-master <sunilair@naver.com>
2020-04-20 18:56:09 +0900

    facebook login test

--- cookies/app_session_mysql.js
+++ cookies/app_session_mysql.js
@@ -11,11 +11,10 @@ var LocalStrategy = require("passport-local").Strategy;
 var FacebookStrategy = require("passport-facebook").Strategy;
 
 
-
-
 app.use(bodyParser.urlencoded({extended: false}));
 
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(session({
     secret : '123432DAFE!SDFAf', // 우리가 만든 애플리케이션이 session id를 암호화시켜서 넣어준다. 
     resave : false,
@@ -30,6 +29,51 @@ app.use(session({
 }));
 
 
+passport.serializeUser(function(user,done){
+
+    console.log('serializeUser', user);
+    done(null,user.authId);
+})
+
+passport.deserializeUser(function(id,done){
+    console.log('deserializeUser', id);
+    for(var i =0; i<users.length; i++){
+        var user = users[i];
+        if(user.authId===id){ // id는 저장된  id
+            return done(null,user);
+        }
+    }
+    done('There is no user.');
+})
+passport.use(new FacebookStrategy({
+    clientID: '218207626293486',
+    clientSecret: '42c34c8ed2a4f8461d8f7d0a51b19ff8',
+    callbackURL: "/auth/facebook/callback",
+    profileFields: ['id','email','gender','link','locale','name','timezone','updated_time','verified','displayName']
+  },
+  function(accessToken, refreshToken, profile, done) {
+      console.log(profile);
+      var authId = 'facebook:'+profile.id;
+      //이미 존재하는 사용자인지 확인하고 push
+      for(var i = 0; i<users.length;i++){
+        var user = users[i];
+        if(user.authId === authId){
+           return done(null,user);
+        }
+
+     var newuser = { 
+          'authId':authId,
+          'displayName':profile.displayName,
+          'email':profile.emails[0].value // 
+      };
+
+      users.push(newuser);
+      done(null,newuser);
+    }   
+
+  }
+  
+));
 
 app.get('/count', function(req,res){
     
@@ -47,8 +91,16 @@ app.get('/count', function(req,res){
 app.get(
     '/auth/facebook',
     passport.authenticate(
-        'facebook'
+        'facebook',
+        {scope : 'email'} // 이메일정보가 필요 scope의 정보가 필요하면 facebook scope정보를 봐봐라.
         ));
+
+app.get('/auth/facebook/callback',
+
+    passport.authenticate(
+        'facebook', 
+        { successRedirect: '/welcome', failureRedirect: '/auth/login' }));  
+
 
 app.get('/auth/logout',function(req,res){
     delete req.session.displayName; //리다이랙션을 하는경우엔 문제가 생길 수 있다.그래서 save 시켜줘야한다.
@@ -77,6 +129,7 @@ app.post('/auth/register',function(req,res){
     hasher({password:req.body.password}, function(err,pass,salt,hash){
 
         var user = {
+            authId:'local:'+req.body.username,
             username: req.body.username,
             password:hash,
             salt:salt,
@@ -127,6 +180,7 @@ app.post('/auth/login', function(req,res){
 
 })
 var users = [{
+    authId: 'local:egoing',
     username : 'egoing',
     password : 'g8BkdM7pZhVp4A5o3FrUOpq0YR14ZXG84SVeCYjK/YBW5hPbTmguaZvc4u817aw8FAS2dyUWBieXp9j01KEni/Q+/do/DsqRyGu4rtfLt3Xpx/V6fHXiNKUuaehBsgleJhZEJKv5QerJtcHoRJpZENWCznvFb95b6z68ff9oEA=', // 평문으로 비밀번호가 저장되어있다.
     salt : "YythG/9ScnZbOTtvx9U3NAavaJDoywcbRw630KjX9flFQsFTaKvV+zZFY+fGJtQaxJljipR5PDIhK1Gbte4bOg==",
